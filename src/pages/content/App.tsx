bbf0e09c9662adca7451d42edc12fcd1b6897833
thsuekfk2 <thsuekfk2@naver.com>
2024-02-09 21:15:03 +0900

    feat : 특정 키, 새로고침 막기

--- src/pages/content/App.tsx
+++ src/pages/content/App.tsx
@@ -1,3 +1,21 @@
 export default function App() {
+  window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 18 || event.key === "Alt") {
+      event.preventDefault();
+      console.log("[key 감지] alt/option key 감지");
+    }
+  });
+
+  window.addEventListener("beforeunload", function (event) {
+    event.preventDefault();
+    event.returnValue = "";
+  });
+
+  window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 116 || (event.ctrlKey && event.keyCode === 82)) {
+      event.preventDefault();
+    }
+  });
+
   return <div>content</div>;
 }
