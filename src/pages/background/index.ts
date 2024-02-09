b2f27f921332704ee4b54d7e892301236044ccb0
thsuekfk2 <thsuekfk2@naver.com>
2024-02-09 21:13:27 +0900

    feat : 현재 탭을 제외한 모든 탭 닫기 및 자동 업데이트 구현

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,1 +1,21 @@
-console.log("background");
+chrome.tabs.query(
+  { currentWindow: true, active: true },
+  function (currentTab: chrome.tabs.Tab[]) {
+    var currentTabId = currentTab[0]?.id;
+
+    chrome.tabs.query(
+      { currentWindow: true },
+      function (tabs: chrome.tabs.Tab[]) {
+        tabs.forEach(function (tab) {
+          if (tab.id !== currentTabId && tab.id !== undefined) {
+            chrome.tabs.remove(tab.id);
+          }
+        });
+      }
+    );
+  }
+);
+
+chrome.runtime.onUpdateAvailable.addListener(() => {
+  chrome.runtime.reload();
+});
